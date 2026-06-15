1bdb9272e98e25e184f9e5122bc20a45e4414ac6
Natalia Gabriely <gabriely.j@escolar.ifrn.edu.br>
2026-06-15 20:25:44 -0300

    dialogo inicial com a mae

--- TCC/Assets/Scripts/Casa/MotherDialogue.cs
+++ TCC/Assets/Scripts/Casa/MotherDialogue.cs
@@ -6,7 +6,6 @@ public class MotherDialogue : MonoBehaviour
 {
     [Header("UI")]
     public GameObject dialoguePanel;
-
     public Image portraitImage;
     public TMP_Text characterNameText;
     public TMP_Text dialogueText;
@@ -16,12 +15,61 @@ public class MotherDialogue : MonoBehaviour
     public Button choice2;
     public Button choice3;
 
-    [Header("Portrait")]
+    [Header("Portraits")]
     public Sprite motherPortrait;
+    public Sprite mariPortrait;
+
+    private CanvasGroup canvasGroup;
 
     private bool dialogueOpen = false;
+    private bool inConversation = false;
 
-    void OnMouseDown()
+    private bool conversou = false;
+    private bool perguntouTrabalho = false;
+
+    private string[] currentLines;
+    private Sprite[] currentPortraits;
+    private string[] currentNames;
+
+    private int currentLine = 0;
+
+    private void Start()
+    {
+        canvasGroup = dialoguePanel.GetComponent<CanvasGroup>();
+
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    private void Update()
+    {
+        if (inConversation)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                NextLine();
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (CursorManager.Instance != null)
+        {
+            CursorManager.Instance.SetLupa();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (CursorManager.Instance != null)
+        {
+            CursorManager.Instance.SetNormal();
+        }
+    }
+
+    private void OnMouseDown()
     {
         if (dialogueOpen)
             return;
@@ -32,44 +80,148 @@ public class MotherDialogue : MonoBehaviour
     void OpenChoices()
     {
         dialogueOpen = true;
+        inConversation = false;
 
-        dialoguePanel.SetActive(true);
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
 
         portraitImage.sprite = motherPortrait;
-
         characterNameText.text = "Mãe";
-
         dialogueText.text = "O que foi, Mari?";
 
-        choice1.GetComponentInChildren<TMP_Text>().text = "Conversar";
-        choice2.GetComponentInChildren<TMP_Text>().text = "Perguntar sobre o trabalho";
+        if (!conversou)
+        {
+            choice1.gameObject.SetActive(true);
+
+            choice1.GetComponentInChildren<TMP_Text>().text = "Conversar";
+
+            choice1.onClick.RemoveAllListeners();
+            choice1.onClick.AddListener(Conversar);
+        }
+        else
+        {
+            choice1.gameObject.SetActive(false);
+        }
+
+        if (!perguntouTrabalho)
+        {
+            choice2.gameObject.SetActive(true);
+
+            choice2.GetComponentInChildren<TMP_Text>().text =
+                "Perguntar sobre o trabalho";
+
+            choice2.onClick.RemoveAllListeners();
+            choice2.onClick.AddListener(PerguntarTrabalho);
+        }
+        else
+        {
+            choice2.gameObject.SetActive(false);
+        }
+
+        choice3.gameObject.SetActive(true);
+
         choice3.GetComponentInChildren<TMP_Text>().text = "Sair";
 
-        choice1.onClick.RemoveAllListeners();
-        choice2.onClick.RemoveAllListeners();
         choice3.onClick.RemoveAllListeners();
-
-        choice1.onClick.AddListener(Conversar);
-        choice2.onClick.AddListener(PerguntarTrabalho);
         choice3.onClick.AddListener(FecharDialogo);
     }
 
     void Conversar()
     {
-        dialogueText.text =
-            "Como foi a escola hoje, filha? Você parece pensativa.";
+        conversou = true;
+
+        currentLines = new string[]
+        {
+            "Como foi a escola hoje, filha? Você parece pensativa.",
+            "Foi normal... Só estou pensando nesse trabalho sobre a árvore genealógica.",
+            "Talvez seja uma boa oportunidade para descobrir coisas que você ainda não conhece sobre a nossa família."
+        };
+
+        currentPortraits = new Sprite[]
+        {
+            motherPortrait,
+            mariPortrait,
+            motherPortrait
+        };
+
+        currentNames = new string[]
+        {
+            "Mãe",
+            "Mari",
+            "Mãe"
+        };
+
+        StartConversation();
     }
 
     void PerguntarTrabalho()
     {
-        dialogueText.text =
-            "Esse trabalho sobre a família pode ser uma boa oportunidade para conhecer mais sobre as nossas origens.";
+        perguntouTrabalho = true;
+
+        currentLines = new string[]
+        {
+            "Mãe... você acha que pode me ajudar com esse trabalho?",
+            "Claro. Temos algumas fotografias antigas guardadas em casa.",
+            "Vou procurar depois. Obrigada."
+        };
+
+        currentPortraits = new Sprite[]
+        {
+            mariPortrait,
+            motherPortrait,
+            mariPortrait
+        };
+
+        currentNames = new string[]
+        {
+            "Mari",
+            "Mãe",
+            "Mari"
+        };
+
+        StartConversation();
+    }
+
+    void StartConversation()
+    {
+        inConversation = true;
+        currentLine = 0;
+
+        choice1.gameObject.SetActive(false);
+        choice2.gameObject.SetActive(false);
+        choice3.gameObject.SetActive(false);
+
+        ShowLine();
+    }
+
+    void ShowLine()
+    {
+        portraitImage.sprite = currentPortraits[currentLine];
+        characterNameText.text = currentNames[currentLine];
+        dialogueText.text = currentLines[currentLine];
+    }
+
+    void NextLine()
+    {
+        currentLine++;
+
+        if (currentLine >= currentLines.Length)
+        {
+            OpenChoices();
+            return;
+        }
+
+        ShowLine();
     }
 
     void FecharDialogo()
     {
-        dialoguePanel.SetActive(false);
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
 
         dialogueOpen = false;
+        inConversation = false;
     }
 }
